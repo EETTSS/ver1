98397751b6d32814a85e119aea08af974aaffad3
elisayhi <sakiomb32309@gmail.com>
2015-10-25 00:01:04 +0800

    ab

--- js/about.js
+++ js/about.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-	$('.i12').mouseenter(function(){
-		$('i12').fadeIn(500,1);
-		});
-	$('.i12').mouseleave(function(){
-		$(this).fadeOut(500,0);
-	});
+	$('.team background-image').fadeTo(0.5);
 });
